017d03f94dca16e12d6855128b6798a17a487887
Chase Clark <chase_clark07@hotmail.com>
2024-01-30 14:38:04 -0500

    add simple CLI functionality

--- convert.py
+++ convert.py
@@ -4,22 +4,30 @@ from pathlib import Path
 
 def main():
     print("starting...")
-    # if (args_count := len(sys.argv)) > 2:
-    #     print(f"Please specify path to .ctb file {args_count - 1}")
-    #     raise SystemExit(2)
-    # elif args_count < 2:
-    #     print("You must specify the target directory")
-    #     raise SystemExit(2)
+    if (args_count := len(sys.argv)) == 2:
+        if sys.argv[1] == "help":
+            print("convert.py /PATH_TO_CHERRYTREE_DB/ /PATH_TO_NEW_DIR/")
+            raise SystemExit(0)
+    elif args_count > 3:
+        print("Too many args: type 'help' for usage")
+        raise SystemExit(2)
+    elif args_count < 3:
+        print("Too few args: type 'help' for usage")
+        raise SystemExit(2)
 
-    # ct_file = Path(sys.argv[1])
+    ct_file = Path(sys.argv[1])
+    target_dir = Path(sys.argv[2])
+    
+    print(f'ct_file:{ct_file} target_dir:{target_dir}')
 
-    # if not target_dir.is_dir():
-    #     print("The target directory doesn't exist")
-    #     raise SystemExit(1)
+    if target_dir.is_dir():
+        print("The target directory exists already! this program will not overwrite! exiting...")
+        raise SystemExit(1)
 
+    #TODO open connection to the cherrytree file and attempt to read nodes
 
-    # for entry in target_dir.iterdir():
-    #     print(entry.name)
+    target_dir.mkdir()
+    print('finished')
 
 if __name__ == "__main__":
     main()
