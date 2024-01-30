7a167adb1ed022c4513dc0201b0cc867f8775f39
Chase Clark <chase_clark07@hotmail.com>
2024-01-29 21:19:59 -0500

    Initial commit

---
+++ convert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+from pathlib import Path
+
+def main():
+    print("starting...")
+    # if (args_count := len(sys.argv)) > 2:
+    #     print(f"Please specify path to .ctb file {args_count - 1}")
+    #     raise SystemExit(2)
+    # elif args_count < 2:
+    #     print("You must specify the target directory")
+    #     raise SystemExit(2)
+
+    # ct_file = Path(sys.argv[1])
+
+    # if not target_dir.is_dir():
+    #     print("The target directory doesn't exist")
+    #     raise SystemExit(1)
+
+
+    # for entry in target_dir.iterdir():
+    #     print(entry.name)
+
+if __name__ == "__main__":
+    main()
