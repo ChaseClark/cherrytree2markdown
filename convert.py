8e30537aacd48d6669ed744e9532eefaa5ff2b4e
Chase Clark <chase_clark07@hotmail.com>
2024-02-04 02:36:14 -0500

    implement basic markdown conversion

--- convert.py
+++ convert.py
@@ -5,6 +5,33 @@ import sqlite3
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
+def to_md(attr: dict, text: str) -> str:
+    if text is None:
+        return ''
+    replaced = text.replace('☐','- [ ]').replace('☑','- [x]')
+    for k in attr.keys():
+        match k:
+            case 'scale':
+                if attr[k][0].lower() == 'h':
+                    return f'{'#'*int(attr[k][1])} {replaced}'
+                elif attr[k].lower() == 'sub':
+                    return f'<sub>{replaced}</sub>'
+                elif attr[k].lower() == 'sup':
+                    return f'<sup>{replaced}</sup>'
+            case 'weight':
+                return f'**{replaced}**'
+            case 'style':
+                return f'*{replaced}*'
+            case 'strikethrough':
+                return f'~~{replaced}~~'
+            case 'underline':
+                return f'=={replaced}=='
+            case 'family':
+                if attr[k].lower() == 'monospace':
+                    return f'`{replaced.split('\n')[0]}`\n\n'
+            case _:
+                return replaced or ''
+    return replaced or ''
 def main():
     connection = sqlite3.connect("ct_db.ctb")
 
@@ -35,9 +62,6 @@ def main():
     if target_dir.exists():
         shutil.rmtree(target_dir)
 
-
-    #TODO open connection to the cherrytree file and attempt to read nodes
-
     cursor = connection.cursor()
     nodes = cursor.execute("SELECT * FROM node").fetchall()
     # print(rows)
@@ -50,13 +74,21 @@ def main():
 
 
     node1 = nodes[0]
-    print(node1[1])
+    # print(node1)
+    # print(f'node1[1]: {node1[1]}')
     root = ET.fromstring(node1[2])
+    count = 0
     for child in root:
-        if len(child.attrib) > 0:
-            print(child.tag, child.attrib, child.text)
+        # print(count,child.tag, child.attrib, child.text)
+        # print(type(child.attrib))
+        count += 1
 
     target_dir.mkdir()
+
+    with open(target_dir.joinpath('new.md'), encoding="utf-8", mode='w') as f:
+        if f.writable():
+            for child in root:
+                f.write(to_md(child.attrib,child.text))
     print('finished')
 
 if __name__ == "__main__":
